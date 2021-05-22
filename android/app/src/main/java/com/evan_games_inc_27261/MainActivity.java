99f379cf9f5cfb94d6c79940ef83bea3d209ca94
Crowdbotics <team@crowdbotics.com>
2021-05-22 21:07:34 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/evan_games_inc_27261/MainActivity.java
@@ -0,0 +1,15 @@
+package com.evan_games_inc_27261;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "evan_games_inc_27261";
+  }
+}
