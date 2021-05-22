16055af3795bc4d6291a6c24d00527d1bf64fb46
Crowdbotics <team@crowdbotics.com>
2021-05-22 21:09:28 +0000

    Update models

--- backend/evan_games_inc_27261/wsgi.py
+++ backend/evan_games_inc_27261/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'evan_games_inc_27261.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "evan_games_inc_27261.settings")
 
 application = get_wsgi_application()
